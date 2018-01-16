0ffe03f9192d5cbef9920a4fa1b3d3ba82e00476
TJ Draper <tj@buzzingpixel.com>
2018-01-16 12:48:40 -0600

    Update ExpressionEngine to 4.0.5

--- src/themes/ee/asset/javascript/compressed/cp/design/routes.js
+++ src/themes/ee/asset/javascript/compressed/cp/design/routes.js
@@ -5,4 +5,4 @@
  * @copyright Copyright (c) 2003-2017, EllisLab, Inc. (https://ellislab.com)
  * @license   https://expressionengine.com/license
  */
-$(document).ready(function(){$(".grid-publish").find(".toolbar .add a").parents("ul.toolbar").remove(),$(".grid-publish").removeClass("grid-publish"),$(".grid-input-form").removeClass("grid-input-form"),$("#routes").on("grid:addRow",function(d,e){$(e).addClass("setting-field"),SelectField.renderFields($(e).find("td").eq(1))})});
+$(document).ready(function(){$(".grid-publish").find(".toolbar .add a").parents("ul.toolbar").remove(),$(".grid-publish").removeClass("grid-publish"),$(".grid-input-form").removeClass("grid-input-form"),$("#routes").on("grid:addRow",function(d,r){$(r).addClass("setting-field"),Dropdown.renderFields($(r).find("td").eq(1))})});
